de08161694a4955eece696b1e85feb0e4ac8b868
devlrxxh <seabearvalorant@gmail.com>
2024-03-30 23:31:30 +0100

    Added lunar ping command

--- src/main/java/me/lrxh/practice/commands/user/PingCommand.java
+++ src/main/java/me/lrxh/practice/commands/user/PingCommand.java
@@ -3,6 +3,8 @@ package me.lrxh.practice.commands.user;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import me.lrxh.practice.Locale;
+import me.lrxh.practice.match.Match;
+import me.lrxh.practice.profile.Profile;
 import me.lrxh.practice.util.BukkitReflection;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -13,13 +15,20 @@ public class PingCommand extends BaseCommand {
 
     @Default
     public void ping(Player player) {
-        player.sendMessage(Locale.PING_YOUR.format(player, BukkitReflection.getPing(player)));
+        Profile profile = Profile.getProfiles().get(player.getUniqueId());
+
+        if(profile.getMatch() != null){
+            Match match = profile.getMatch();
+            player.sendMessage(Locale.PING_YOUR.format(player, BukkitReflection.getPing(player)));
+            player.sendMessage(Locale.PING_OTHERS.format(player, BukkitReflection.getPing(match.getOpponent(player)), match.getOpponent(player).getName()));
+        }else {
+            player.sendMessage(Locale.PING_YOUR.format(player, BukkitReflection.getPing(player)));
+        }
     }
 
     @Default
     @Syntax("<name>")
     @CommandCompletion("@names")
     public void pingOthers(Player player, String otherPlayer) {
-        player.sendMessage(Locale.PING_OTHERS.format(player, BukkitReflection.getPing(Bukkit.getPlayer(otherPlayer)), Bukkit.getPlayer(otherPlayer).getName()));
     }
 }
